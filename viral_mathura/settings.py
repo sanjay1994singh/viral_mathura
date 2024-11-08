7cd1b6ae3b5ab5b2f5dcb12bc3db6f7d1802e803
sanjay <sanjaypython22@gmail.com>
2024-11-08 13:02:11 +0530

    p

--- viral_mathura/settings.py
+++ viral_mathura/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'viral_mathura.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'mb/db.sqlite3',
     }
 }
 
