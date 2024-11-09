8d820aee72cc30714137a4646bb1055885f15e16
sanjay <sanjaypython22@gmail.com>
2024-11-09 10:46:46 +0530

    p

--- video/views.py
+++ video/views.py
@@ -6,7 +6,11 @@ from video.models import Video
 # Create your views here.
 def video_detail(request, id):
     news = Video.objects.get(id=id)
-    absolute_image_url = request.build_absolute_uri(news.featured_image.url)
+    try:
+        absolute_image_url = request.build_absolute_uri(news.featured_image.url)
+    except:
+        absolute_image_url = ''
+
     context = {
         'news': news,
         'absolute_image_url': absolute_image_url,
